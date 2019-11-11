b20d27f50926c1c9439d59e3dc7cdcae95aa282c
kritikhullar <50941519+kritikhullar@users.noreply.github.com>
2019-11-11 10:12:45 +0530

    Add files via upload

---
+++ Common/Table.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Assessment_EF.Models;
+
+namespace Assessment_EF.Common
+{
+    public class Table
+    {
+        public IEnumerable<Employee> emp { get; set; }
+        public IEnumerable<Department> dept { get; set; }
+
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_EF.Models;
+using Assessment_EF.Common;
+
+namespace Assessment_EF.Controllers
+{
+    public class HomeController : Controller
+    {
+        FormsAuthenticationDbEntities context = new FormsAuthenticationDbEntities();
+
+        //GET:Home
+        public ActionResult Index()
+        {
+            var table = new Table();
+            table.emp = context.Employees.ToList();
+            table.dept = context.Departments.ToList();
+
+            return View(table);
+        }
+
+        
+    }
+}
